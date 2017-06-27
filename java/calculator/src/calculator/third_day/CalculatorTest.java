8cf9295beeb02efb8e6b9b39ce4ddaed084124b9
chengwei07 <chengwei07@baidu.com>
2017-06-27 15:04:33 +0800

    refactor

--- java/calculator/src/calculator/third_day/CalculatorTest.java
+++ java/calculator/src/calculator/third_day/CalculatorTest.java
@@ -8,15 +8,14 @@ import static org.junit.Assert.assertEquals;
  * Created by chengwei on 2017/6/27.
  */
 public class CalculatorTest {
+    private Calculator calculator = new Calculator();
     @Test
     public void input_empty_string_return_0() {
-        Calculator calculator = new Calculator();
         assertEquals(0, calculator.calculate(""));
     }
 
     @Test
     public void input_string_1__return_1() {
-        Calculator calculator = new Calculator();
         assertEquals(1, calculator.calculate("1"));
     }
 }
