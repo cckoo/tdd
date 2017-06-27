4df64658cf24cea01c67911bc86a4756a62891db
chengwei07 <chengwei07@baidu.com>
2017-06-27 15:17:37 +0800

    refactor

--- java/calculator/src/calculator/third_day/Calculator.java
+++ java/calculator/src/calculator/third_day/Calculator.java
@@ -6,14 +6,27 @@ import java.util.Arrays;
  * Created by chengwei on 2017/6/27.
  */
 public class Calculator {
+    private static final String DELIMITER = ",";
     public int calculate(String s) {
         if (s.isEmpty())
             return 0;
-        if (s.contains(",")) {
-            String[] stringNums = s.split(",");
-            int[] nums = Arrays.stream(stringNums).mapToInt(Integer::parseInt).toArray();
-            return Arrays.stream(nums).sum();
-        }
+
+        if (s.contains(DELIMITER))
+            return getSumWithDelimiter(s);
+
         return Integer.parseInt(s);
     }
+
+    private int getSumWithDelimiter(String s) {
+        return Arrays.stream(getIntegerArrayBy(splitStringToStringArray(s))).sum();
+    }
+
+    private int[] getIntegerArrayBy(String[] stringArray) {
+        return Arrays.stream(stringArray).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private String[] splitStringToStringArray(String s) {
+        return s.split(DELIMITER);
+    }
+
 }
