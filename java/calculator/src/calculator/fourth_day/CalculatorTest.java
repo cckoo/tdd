fa9adfba0a59556abc305fd818e8426c480507f1
cckoo <595743688@qq.com>
2017-06-28 14:40:30 +0800

    Add test input_empty_string_return_0 and can not find method calculate

--- java/calculator/src/calculator/fourth_day/CalculatorTest.java
+++ java/calculator/src/calculator/fourth_day/CalculatorTest.java
@@ -1,7 +1,16 @@
 package calculator.fourth_day;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by chengwei on 2017/6/28.
  */
 public class CalculatorTest {
+    @Test
+    public void input_empty_string_return_0() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.calculate(""));
+    }
 }
