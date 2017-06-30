7e52c57e08a255bed9df11eb3b36ac3e0a857313
cckoo <595743688@qq.com>
2017-06-30 11:05:20 +0800

    Initial project and add test input_empty_string_return_0 run with error can not find method calculate

---
+++ java/calculator/src/calculator/fifth_day/Calculator.java
@@ -0,0 +1,7 @@
+package calculator.fifth_day;
+
+/**
+ * Created by chengwei on 2017/6/30.
+ */
+public class Calculator {
+}
---
+++ java/calculator/src/calculator/fifth_day/CalculatorTest.java
@@ -0,0 +1,16 @@
+package calculator.fifth_day;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chengwei on 2017/6/30.
+ */
+public class CalculatorTest {
+    @Test
+    public void input_empty_string_return_0() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.calculate(""));
+    }
+}
