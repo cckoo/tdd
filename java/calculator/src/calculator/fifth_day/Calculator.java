5542d2833bef6a00df2a414e7671385d8d3c4e7d
cckoo <595743688@qq.com>
2017-06-30 11:18:20 +0800

    Pass test

--- java/calculator/src/calculator/fifth_day/Calculator.java
+++ java/calculator/src/calculator/fifth_day/Calculator.java
@@ -1,5 +1,7 @@
 package calculator.fifth_day;
 
+import java.util.Arrays;
+
 /**
  * Created by chengwei on 2017/6/30.
  */
@@ -7,6 +9,11 @@ public class Calculator {
     public int calculate(String s) {
         if (s.isEmpty())
             return 0;
+        if (s.contains(",")) {
+            String[] stringNums = s.split(",");
+            int[] nums = Arrays.stream(stringNums).mapToInt(Integer::parseInt).toArray();
+            return Arrays.stream(nums).sum();
+        }
         return Integer.parseInt(s);
     }
 }
